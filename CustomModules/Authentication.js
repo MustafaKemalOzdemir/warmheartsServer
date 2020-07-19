b948f8ff8571e4bbd4ec07798e18439db6081e44
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-07-19 18:34:09 +0300

    Token creation is moved to the Authentication module

--- CustomModules/Authentication.js
+++ CustomModules/Authentication.js
@@ -2,12 +2,20 @@ const keys = require('../private/keys');
 
 let TokenCheck = (token) => {
     try {
-        return  { 'Success': true, 'token' : jwt.verify(token, keys.jwtKey)};
+        return { 'Success': true, 'token': jwt.verify(token, keys.jwtKey) };
     } catch (error) {
-        return{ 'Success': false, 'error' : error}
+        return { 'Success': false, 'error': error }
     }
 }
 
+let TokenCreate = (id, expiryDate) => {
+    return jwt.sign(
+        { userId: id },
+        keys.jwtKey,
+        { expiresIn: expiryDate }
+    );
+}
 module.exports = {
-    TokenCheck
+    TokenCheck,
+    TokenCreate
 }
