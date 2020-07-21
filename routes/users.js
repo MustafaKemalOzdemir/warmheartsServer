7261daf04695cda5e4a0c0ae93ad24baa06678b6
Emre Can HACIOSMANOĞLU <emrecan.haciosmanoglu@agu.edu.tr>
2020-07-21 19:00:09 +0300

    /users/edit route is implemented

--- routes/users.js
+++ routes/users.js
@@ -94,4 +94,78 @@ router.post('/signin', async (req, res) => {
     });
 });
 
+router.post('/edit', async (req, res) => {
+
+    const { email, password, token, newEmail, newPassword, newFirstname, newLastname } = req.body;
+
+    if (!email)
+        return res.status(400).json({
+            'success': false,
+            'message': 'Email has not been provided'
+        });
+    if (!password)
+        return res.status(400).json({
+            'success': false,
+            'message': 'Password has not been provided'
+        });
+    if (!token)
+        return res.status(400).json({
+            'success': false,
+            'message': 'Token has not been provided'
+        });
+
+    var tokenResult = Auth.TokenCheck(token);
+
+    if (tokenResult.Success) {
+        var userCheckResult = Auth.UserCheck(user.userId, tokenResult.token);
+        if (userCheckResult.Success)
+            User.findOne({ 'email': email }, (user) => {
+                if (!user)
+                    return res.status(400).json({
+                        'Success': false,
+                        'message': 'Unathorized access'
+                    });
+                if (user.password !== password)
+                    return res.status(400).json({
+                        'Success': false,
+                        'message': 'Unathorized access'
+                    });
+
+                if (newEmail)
+                    User.email = newEmail;
+                if (newPassword)
+                    User.password = newPassword;
+                if (newFirstname)
+                    User.firstname = newFirstname;
+                if (newLastname)
+                    User.lastname = newLastname;
+
+                user.save().then((result) => {
+                    if (result)
+                        return res.status(200).json({
+                            'success': true,
+                            'message': 'User has been updated',
+                            'user': result
+                        });
+                }).catch((error) => {
+                    if (error)
+                        return res.status(500).json({
+                            'success': false,
+                            'message': error
+                        });
+                });
+            });
+        else
+            return res.status(400).json({
+                'Success': false,
+                'message': 'Unathorized access'
+            });
+    }
+    else
+        return res.status(401).json({
+            'Success': false,
+            'message': 'Unathorized access'
+        });
+});
+
 module.exports = router;
